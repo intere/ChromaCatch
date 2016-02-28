2704a8e532626b2a945b16f6c8dd378c1d567a8c
Eric Internicola <einternicola@silverchalice.com>
2016-02-28 10:37:17 -0700

    Initial Commit.  The Chroma Catch Game.

--- ChromaCatch/Controllers/GameViewController.swift
+++ ChromaCatch/Controllers/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
+        if let scene = TitleScene(fileNamed:"TitleScene") {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
@@ -24,7 +24,7 @@ class GameViewController: UIViewController {
             skView.ignoresSiblingOrder = true
             
             /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+            scene.scaleMode = .ResizeFill
             
             skView.presentScene(scene)
         }
